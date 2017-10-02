1ec12729f2bbcec66faa0eedcf26fcb8489aee66
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-02 12:49:31 +0100

    added typing effect

---
+++ js/typing.js
@@ -0,0 +1,31 @@
+$('#current-type').typeIt({
+    strings: 'Current',
+    speed: 50,
+    autoStart: false,
+    lifeLike: true,
+    html: false
+});
+
+$('#past-type').typeIt({
+    strings: 'Past',
+    speed: 50,
+    autoStart: false,
+    lifeLike: true,
+    html: false
+});
+
+$('#aboutus-type').typeIt({
+    strings: 'About us',
+    speed: 50,
+    autoStart: false,
+    lifeLike: true,
+    html: false
+});
+
+$('#contactus-type').typeIt({
+    strings: 'Contact us',
+    speed: 50,
+    autoStart: false,
+    lifeLike: true,
+    html: false
+});
